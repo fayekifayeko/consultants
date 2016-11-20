f0f0ee85eedde6453590b261fd882585a5aa4095
faeq <fayekmir@gmail.com>
2016-11-20 11:54:18 -0800

    Adding Model Classes Consultant and Assignment

---
+++ consultants/consultants/Models/Assignment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace consultants.Models
+{
+    public class Assignment
+    {
+        public int Assignment_ID { get; set; }
+        public string Assignment_Name { get; set; }
+
+        public string Client_Name { get; set; }
+        public string Percentage { get; set; }
+        public DateTime Start_Date { get; set; }
+        public DateTime End_Date { get; set; }
+        public string Comment { get; set; }
+        public int Consultant_ID { get; set; }
+
+        public virtual Consultant Consultant { get; set; }
+    }
+}
---
+++ consultants/consultants/Models/Consultant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace consultants.Models
+{
+    public class Consultant
+    {
+        public int Consultant_ID { get; set; }
+        public string Name { get; set; }
+       
+        public virtual ICollection<Assignment> Enrollments { get; set; }
+    }
+}
